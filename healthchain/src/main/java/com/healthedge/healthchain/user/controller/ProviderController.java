0ad810349897594bba9f8a46ce1a12d6f1d5eb36
lakshit1307 <lakshit1307@gmail.com>
2020-03-05 14:11:34 +0530

    Created a controller service structure

--- healthchain/src/main/java/com/healthedge/healthchain/user/controller/ProviderController.java
+++ healthchain/src/main/java/com/healthedge/healthchain/user/controller/ProviderController.java
@@ -1,40 +1,63 @@
 package com.healthedge.healthchain.user.controller;
 
+import com.healthedge.healthchain.user.dto.BaseResponse;
 import com.healthedge.healthchain.user.dto.BenefitPlanRequest;
-import com.healthedge.healthchain.user.dto.UserDTO;
 import com.healthedge.healthchain.user.entity.BenefitPlan;
 import com.healthedge.healthchain.user.entity.Member;
+import com.healthedge.healthchain.user.service.impl.ProviderServiceImpl;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "v1.0/provider")
 public class ProviderController {
 
-    @ApiOperation(value = "view a user")
-    @PostMapping(value = "/user/{userId}")
+    @Autowired
+    private ProviderServiceImpl providerService;
+
+    @ApiOperation(value = "create a benefit plan")
+    @PostMapping(value = "/benefitPlan")
     @ResponseBody
-    public ResponseEntity createBenefitPlan(@RequestBody BenefitPlanRequest benefitPlan) {
-        return new ResponseEntity(userDTO, HttpStatus.OK);
+    public ResponseEntity<BaseResponse> createBenefitPlan(@RequestBody BenefitPlanRequest benefitPlan) {
+        BaseResponse baseResponse = providerService.createBenefitPlan(benefitPlan);
+        return new ResponseEntity(baseResponse, HttpStatus.OK);
     }
 
-    public ResponseEntity createMember(@RequestBody Member member){
-
+    @ApiOperation(value = "create a member")
+    @PostMapping(value = "/member")
+    @ResponseBody
+    public ResponseEntity<BaseResponse> createMember(@RequestBody Member member) {
+        BaseResponse baseResponse = providerService.createBenefitPlan(null);
+        return new ResponseEntity(baseResponse, HttpStatus.OK);
     }
 
-    public ResponseEntity updateBenefitPlans(@RequestBody BenefitPlanRequest benefitPlanRequest){
-
+    @ApiOperation(value = "update a benefit plan")
+    @PutMapping(value = "/benefitPlan")
+    @ResponseBody
+    public ResponseEntity<BaseResponse> updateBenefitPlans(@RequestBody BenefitPlanRequest benefitPlanRequest) {
+        BaseResponse baseResponse = providerService.createBenefitPlan(null);
+        return new ResponseEntity(baseResponse, HttpStatus.OK);
     }
 
-
-    public getAllBenefitPlansForProvider(){
-
+    @ApiOperation(value = "get all the benefit plans")
+    @GetMapping(value = "/benefitPlan")
+    @ResponseBody
+    public ResponseEntity<List<BenefitPlan>> getAllBenefitPlansForProvider() {
+        List<BenefitPlan> benefitPlans = providerService.getAllBenefitPlans();
+        return new ResponseEntity(benefitPlans, HttpStatus.OK);
     }
 
-
-    public getBenefitPlanHistory(String benefitPlanId) {
+    @ApiOperation(value = "get a the benefit plan")
+    @GetMapping(value = "/benefitPlan/{benefitPlanId}")
+    @ResponseBody
+    public ResponseEntity<List<BenefitPlan>> getBenefitPlanHistory(@PathVariable("benefitPlanId") String benefitPlanId) {
+        List<BenefitPlan> benefitPlans = providerService.getBenefitPlanHistory(benefitPlanId);
+        return new ResponseEntity(benefitPlans, HttpStatus.OK);
     }
 
 }
--- healthchain/src/main/java/com/healthedge/healthchain/user/service/impl/ProviderServiceImpl.java
+++ healthchain/src/main/java/com/healthedge/healthchain/user/service/impl/ProviderServiceImpl.java
@@ -1,12 +1,49 @@
 package com.healthedge.healthchain.user.service.impl;
 
+import com.healthedge.healthchain.common.util.Constants;
+import com.healthedge.healthchain.user.dto.BaseResponse;
+import com.healthedge.healthchain.user.dto.BenefitPlanRequest;
 import com.healthedge.healthchain.user.entity.BenefitPlan;
+import com.healthedge.healthchain.user.entity.Member;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ProviderServiceImpl {
 
-    public void createBenefitPlan(BenefitPlan benefitPlan){
+    public BaseResponse createBenefitPlan(BenefitPlanRequest benefitPlan){
+        //TODO Create a benefit plan
 
+
+        BaseResponse baseResponse=new BaseResponse(Constants.SUCCESS, "Benefit Plan successfully created");
+        return baseResponse;
+    }
+
+    public BaseResponse updateBenefitPlan(BenefitPlanRequest benefitPlan){
+        //TODO update a benefit plan
+
+
+        BaseResponse baseResponse=new BaseResponse(Constants.SUCCESS, "Benefit Plan successfully updated");
+        return baseResponse;
+    }
+
+    public BaseResponse createMember(Member member){
+        //TODO Create a member
+        BaseResponse baseResponse=new BaseResponse(Constants.SUCCESS, "Member successfully created");
+        return baseResponse;
+    }
+
+    public List<BenefitPlan> getAllBenefitPlans(){
+        //TODO
+        List<BenefitPlan> benefitPlans= new ArrayList<>();
+        return benefitPlans;
+    }
+
+    public List<BenefitPlan> getBenefitPlanHistory(String benefitPlanId){
+        //TODO
+        List<BenefitPlan> benefitPlans= new ArrayList<>();
+        return benefitPlans;
     }
 }
