64c807d9cbe30afb901918d078b8b52d2cb007ef
lakshit1307 <lakshit1307@gmail.com>
2020-03-05 14:46:36 +0530

    Created a controller service structure

--- healthchain/src/main/java/com/healthedge/healthchain/user/controller/MemberController.java
+++ healthchain/src/main/java/com/healthedge/healthchain/user/controller/MemberController.java
@@ -1,14 +1,25 @@
 package com.healthedge.healthchain.user.controller;
 
+import com.healthedge.healthchain.user.entity.BenefitPlan;
+import com.healthedge.healthchain.user.service.impl.MemberServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "v1.0/member")
 public class MemberController {
 
+    @Autowired
+    private MemberServiceImpl memberService;
 
-    public getBenifitPlansForMember(String memberId){
 
+    public ResponseEntity<List<BenefitPlan>> getBenifitPlansForMember(String memberId) {
+        List<BenefitPlan> benefitPlans = memberService.getBenifitPlansForMember(memberId);
+        return new ResponseEntity(benefitPlans, HttpStatus.OK);
     }
 }
